d00a627dd55619245de671ad979215d19da35aaa
Leonardo G <lgomes.leo@gmail.com>
2018-10-23 01:12:14 -0300

    evaluate ready (?)

--- src/evaluator.c
+++ src/evaluator.c
@@ -7,8 +7,14 @@
 #include "eu_rt.h"
 
 eu_result eu_evaluate(europa* s, eu_value* v, eu_value* out) {
-	/* compile the value */
+	eu_value chunk;
 
+	/* compile the value */
+	_eu_checkreturn(eucode_compile(s, v, &chunk));
+
+	/* run it in the vm */
+	_eu_checkreturn(euvm_doclosure(s, _euvalue_to_closure(&chunk), &_null,
+		out));
 
 	return EU_RESULT_OK;
 }
